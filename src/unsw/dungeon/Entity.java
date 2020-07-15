1a3d0dae62e74a63618432abc9c593fa682bbf1c
Fergus Yip <z5248282@unsw.edu.au>
2020-07-15 18:06:41 +1000

    Add Coordinate, Entity, and MoveableEntity

--- src/unsw/dungeon/Entity.java
+++ src/unsw/dungeon/Entity.java
@@ -1,42 +1,31 @@
 package unsw.dungeon;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-
 /**
  * An entity in the dungeon.
+ * 
  * @author Robert Clifton-Everest
  *
  */
-public class Entity {
+public abstract class Entity {
 
-    // IntegerProperty is used so that changes to the entities position can be
-    // externally observed.
-    private IntegerProperty x, y;
+    private Coordinate position;
+    private Dungeon dungeon;
 
-    /**
-     * Create an entity positioned in square (x,y)
-     * @param x
-     * @param y
-     */
-    public Entity(int x, int y) {
-        this.x = new SimpleIntegerProperty(x);
-        this.y = new SimpleIntegerProperty(y);
+    public Entity(int x, int y, Dungeon dungeon) {
+        position = new Coordinate(x, y);
+        this.dungeon = dungeon;
     }
 
-    public IntegerProperty x() {
-        return x;
+    public Coordinate getCoordinate() {
+        return position;
     }
 
-    public IntegerProperty y() {
-        return y;
+    public Dungeon getDungeon() {
+        return dungeon;
     }
 
-    public int getY() {
-        return y().get();
+    public Entity interact(Entity caller) {
+        return this;
     }
 
-    public int getX() {
-        return x().get();
-    }
 }
---
+++ src/unsw/dungeon/MoveableEntity.java
@@ -0,0 +1,12 @@
+package unsw.dungeon;
+
+public interface MoveableEntity {
+
+    public void moveUp();
+
+    public void moveDown();
+
+    public void moveLeft();
+
+    public void moveRight();
+}
