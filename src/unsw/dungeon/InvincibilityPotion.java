0ffea3c00ebd244bcb6fbb3401d9a2850c1eeaad
Fergus Yip <z5248282@unsw.edu.au>
2020-07-16 10:29:21 +1000

    Removed id field

--- src/unsw/dungeon/InvincibilityPotion.java
+++ src/unsw/dungeon/InvincibilityPotion.java
@@ -5,20 +5,15 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class InvincibilityPotion extends Entity {
-    private int id;
+
     private boolean isActive;
     private Timer remainingTime;
     private static int maxItem;
 
-    public InvincibilityPotion(int x, int y, Dungeon dungeon, int id) {
+    public InvincibilityPotion(int x, int y, Dungeon dungeon) {
         super(x, y, dungeon);
-        this.id = id;
         this.isActive = false;
         // this.remainingTime = NEED TO SET IT TO A PARTICULAR TIME.
-    }
-
-    public int getID() {
-        return this.id;
     }
 
     public void updateStatus(boolean newActive) {
