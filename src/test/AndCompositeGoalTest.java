737c68aa96a0cc4fe7c7d6a53835d49b984a3661
Fergus Yip <z5248282@unsw.edu.au>
2020-07-18 20:25:15 +1000

    Add test for nested AND goals

--- src/test/AndCompositeGoalTest.java
+++ src/test/AndCompositeGoalTest.java
@@ -61,4 +61,95 @@ class AndCompositeGoalTest {
         assertTrue(dungeon.isComplete());
     }
 
+    @Test
+    void testNestedGoals() {
+        JSONObject playerJSON = new JSONObject();
+        playerJSON.put("x", 0);
+        playerJSON.put("y", 0);
+        playerJSON.put("type", "player");
+
+        JSONObject treasureJSON = new JSONObject();
+        treasureJSON.put("x", 1);
+        treasureJSON.put("y", 0);
+        treasureJSON.put("type", "treasure");
+
+        JSONObject boulderJSON = new JSONObject();
+        boulderJSON.put("x", 2);
+        boulderJSON.put("y", 0);
+        boulderJSON.put("type", "boulder");
+
+        JSONObject switchJSON = new JSONObject();
+        switchJSON.put("x", 3);
+        switchJSON.put("y", 0);
+        switchJSON.put("type", "switch");
+
+        JSONObject exitJSON = new JSONObject();
+        exitJSON.put("x", 2);
+        exitJSON.put("y", 1);
+        exitJSON.put("type", "exit");
+
+        JSONArray entitiesJSON = new JSONArray();
+        entitiesJSON.put(playerJSON);
+        entitiesJSON.put(treasureJSON);
+        entitiesJSON.put(boulderJSON);
+        entitiesJSON.put(switchJSON);
+        entitiesJSON.put(exitJSON);
+
+        JSONObject exitGoal = new JSONObject();
+        exitGoal.put("goal", "exit");
+
+        JSONObject treasureGoal = new JSONObject();
+        treasureGoal.put("goal", "treasure");
+
+        JSONObject switchGoal = new JSONObject();
+        switchGoal.put("goal", "boulders");
+
+        JSONArray subgoals0 = new JSONArray();
+        subgoals0.put(treasureGoal);
+        subgoals0.put(switchGoal);
+
+        JSONObject andGoal0 = new JSONObject();
+        andGoal0.put("goal", "AND");
+        andGoal0.put("subgoals", subgoals0);
+
+        JSONArray subgoals1 = new JSONArray();
+        subgoals1.put(andGoal0);
+        subgoals1.put(exitGoal);
+
+        JSONObject andGoal1 = new JSONObject();
+        andGoal1.put("goal", "AND");
+        andGoal1.put("subgoals", subgoals1);
+
+        JSONObject json = new JSONObject();
+        json.put("width", 4);
+        json.put("height", 2);
+        json.put("entities", entitiesJSON);
+        json.put("goal-condition", andGoal1);
+
+        DungeonLoader loader = new DungeonMockLoader(json);
+        Dungeon dungeon = loader.load();
+        Player player = dungeon.getPlayer();
+
+        // Test that goal isn't complete yet
+        assertFalse(dungeon.isComplete());
+
+        // Move right to collect treasure
+        player.moveRight();
+
+        // Test that goal isn't complete yet
+        assertFalse(dungeon.isComplete());
+
+        // Move right to push boulder onto switch
+        player.moveRight();
+
+        // Test that goal isn't complete yet
+        assertFalse(dungeon.isComplete());
+
+        // Move down to exit
+        player.moveDown();
+
+        // Test that goal has been completed
+        assertTrue(dungeon.isComplete());
+    }
+
 }
