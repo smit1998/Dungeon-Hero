dd56d9415abd93cf8074d169d706c997777171a4
Fergus Yip <z5248282@unsw.edu.au>
2020-07-18 17:39:44 +1000

    Add portal

--- src/unsw/dungeon/Portal.java
+++ src/unsw/dungeon/Portal.java
@@ -18,20 +18,60 @@ public class Portal extends Entity {
         return this.pair;
     }
 
-    public boolean addPair(Portal pair) {
-        // TODO Check not itself        
-        if(pair.getCoordinate() == this.getCoordinate()) {
-            return false;
-        }
-        // TODO Check no pair
-        if(this.pair != null) {
-            return false;
-        }
-
-        this.pair = pair;
-        return true;
-
+    public boolean hasPair() {
+        return pair != null;
     }
 
+    public boolean addPair(Portal pair) {
+        if (this == pair) {
+            return false;
+        }
+        if (getID() != pair.getID()) {
+            return false;
+        }
+        this.pair = pair;
+        return true;
+    }
+
+    public boolean interact(Entity caller) {
+        if (caller instanceof Player) {
+            int oldX = getX();
+            int oldY = getY();
+
+            int newX = getX() - caller.getX() + pair.getX();
+            int newY = getY() - caller.getY() + pair.getY();
+            if (dungeon().interact(caller, newX, newY)) {
+                if (newX >= 0 && newX < dungeon().getWidth()) {
+                    caller.x().set(newX);
+                }
+                if (newY >= 0 && newY < dungeon().getHeight()) {
+                    caller.y().set(newY);
+                }
+            } else {
+                caller.x().set(pair.getX());
+                caller.y().set(pair.getY());
+                if (dungeon().interact(caller, newX, newY)) {
+                    if (newX >= 0 && newX < dungeon().getWidth()) {
+                        caller.x().set(newX);
+                    }
+                    if (newY >= 0 && newY < dungeon().getHeight()) {
+                        caller.y().set(newY);
+                    }
+                }
+            }
+        } else if (caller instanceof Boulder) {
+            int newX = getX() - caller.getX() + pair.getX();
+            int newY = getY() - caller.getY() + pair.getY();
+            if (dungeon().interact(caller, newX, newY)) {
+                if (newX >= 0 && newX < dungeon().getWidth()) {
+                    caller.x().set(newX);
+                }
+                if (newY >= 0 && newY < dungeon().getHeight()) {
+                    caller.y().set(newY);
+                }
+            }
+        }
+        return false;
+    }
 
 }
