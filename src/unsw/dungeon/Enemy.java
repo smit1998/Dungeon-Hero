3da967ca3b4c8af4804be93dd24445d48ad9fe45
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-07-18 17:43:41 +1000

    added feature: enemy can kill unarmed player, and both enemy and player disappears when killed

--- src/unsw/dungeon/Enemy.java
+++ src/unsw/dungeon/Enemy.java
@@ -84,11 +84,21 @@ public class Enemy extends MoveableEntity implements Subject {
         }
     }
 
+    public void attack(Player p) {
+        p.updateLifeStatus(false);
+        p.setVisibility(false);
+    }
+
     @Override
     public boolean interact(Entity caller) {
         if (caller instanceof Player) {
-            Player p = (Player) caller; 
-            return p.attack(this); 
+            Player player = (Player) caller;
+            if (player.attack(this) == true) {
+                return true;
+            } else {
+                this.attack(player);
+                return false;
+            }
         }
         return false;
     }
@@ -98,8 +108,6 @@ public class Enemy extends MoveableEntity implements Subject {
 
     public void updateLifeStatus(boolean newLifeStatus) {
         this.isAlive = newLifeStatus;
-        this.x().set(-1);
-        this.y().set(-1); 
     }
 
     public boolean getIsAlive() {
@@ -107,20 +115,17 @@ public class Enemy extends MoveableEntity implements Subject {
     }
 
     public void attach(Observer o) {
-        if (!observers.contains(o)) {
-            observers.add(o);
-        }
-
+        observers.add(o); 
     }
 
     public void detach(Observer o) {
-        observers.remove(o);
-
+        observers.remove(o); 
     }
 
     public void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update(this);
+        for (Observer obs : observers) {
+            obs.update(this); 
         }
     }
+
 }
