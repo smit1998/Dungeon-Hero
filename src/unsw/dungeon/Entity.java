fd7cbdfa01a4bbfd719681c28dbc9804b3966c41
Fergus Yip <z5248282@unsw.edu.au>
2020-07-20 13:59:17 +1000

    Move coordinate back to entity

--- src/unsw/dungeon/Entity.java
+++ src/unsw/dungeon/Entity.java
@@ -1,7 +1,8 @@
 package unsw.dungeon;
 
-import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
 /**
@@ -12,32 +13,28 @@ import javafx.beans.property.SimpleBooleanProperty;
  */
 public abstract class Entity {
 
-    private Coordinate position;
+    // IntegerProperty is used so that changes to the entities position can be
+    // externally observed.
+    private IntegerProperty x, y;
     private Dungeon dungeon;
-
     private BooleanProperty isVisible;
 
     /**
      * Constructor for entity
-     * @param x coordinate position of entity
-     * @param y coordinate position of entity
-     * @param dungeon entity belongs to 
+     * 
+     * @param x       coordinate position of entity
+     * @param y       coordinate position of entity
+     * @param dungeon entity belongs to
      */
     public Entity(int x, int y, Dungeon dungeon) {
-        position = new Coordinate(x, y);
+        this.x = new SimpleIntegerProperty(x);
+        this.y = new SimpleIntegerProperty(y);
         this.dungeon = dungeon;
         isVisible = new SimpleBooleanProperty(true);
     }
 
     /**
-     * @return coordinates of the entity
-     */
-    public Coordinate getCoordinate() {
-        return position;
-    }
-
-    /**
-     * @return dungeon the entity belongs to 
+     * @return dungeon the entity belongs to
      */
     public Dungeon dungeon() {
         return dungeon;
@@ -45,26 +42,28 @@ public abstract class Entity {
 
     /**
      * Abstract method to implemented in the entity
-     * @return a boolean to whether the calling entity is able to interact with the current one
+     * 
+     * @return a boolean to whether the calling entity is able to interact with the
+     *         current one
      */
     public boolean interact(Entity caller) {
         return false;
     }
 
     public IntegerProperty x() {
-        return position.x();
+        return x;
     }
 
     public IntegerProperty y() {
-        return position.y();
+        return y;
     }
 
     public int getY() {
-        return position.getY();
+        return y().get();
     }
 
     public int getX() {
-        return position.getX();
+        return x().get();
     }
 
     public void setX(int x) {
