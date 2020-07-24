d2ccdd0b1ca5cb53bb0206d36b53cdae458174c9
Fergus Yip <z5248282@unsw.edu.au>
2020-07-25 09:24:36 +1000

    Clean asset loading

--- src/unsw/dungeon/DungeonControllerLoader.java
+++ src/unsw/dungeon/DungeonControllerLoader.java
@@ -22,57 +22,29 @@ import java.io.File;
  */
 public class DungeonControllerLoader extends DungeonLoader {
 
-    private List<ImageView> entities;
+    private List<ImageView> entities = new ArrayList<>();
 
     // Images
-    private Image playerImage;
-    private Image wallImage;
-    private Image exitImage;
-    private Image treasureImage;
-    private Image openDoorImage;
-    private Image closedDoorImage;
-    private Image keyImage;
-    private Image boulderImage;
-    private Image switchImage;
-    private Image portalImage;
-    private Image enemyImage;
-    private Image swordImage;
-    private Image invincibilityImage;
+    private Image playerImage = new Image((new File("images/human_new.png")).toURI().toString());
+    private Image wallImage = new Image((new File("images/brick_brown_0.png")).toURI().toString());
+    private Image exitImage = new Image((new File("images/exit.png")).toURI().toString());
+    private Image treasureImage = new Image((new File("images/gold_pile.png")).toURI().toString());
+    private Image openDoorImage = new Image((new File("images/open_door.png")).toURI().toString());
+    private Image closedDoorImage = new Image((new File("images/closed_door.png")).toURI().toString());
+    private Image keyImage = new Image((new File("images/key.png")).toURI().toString());
+    private Image boulderImage = new Image((new File("images/boulder.png")).toURI().toString());
+    private Image switchImage = new Image((new File("images/pressure_plate.png")).toURI().toString());
+    private Image portalImage = new Image((new File("images/portal.png")).toURI().toString());
+    private Image enemyImage = new Image((new File("images/deep_elf_master_archer.png")).toURI().toString());
+    private Image swordImage = new Image((new File("images/greatsword_1_new.png")).toURI().toString());
+    private Image invincibilityImage = new Image((new File("images/brilliant_blue_new.png")).toURI().toString());
 
     public DungeonControllerLoader(String filename) throws FileNotFoundException {
         super(filename);
-        entities = new ArrayList<>();
-        playerImage = new Image((new File("images/human_new.png")).toURI().toString());
-        wallImage = new Image((new File("images/brick_brown_0.png")).toURI().toString());
-        exitImage = new Image((new File("images/exit.png")).toURI().toString());
-        treasureImage = new Image((new File("images/gold_pile.png")).toURI().toString());
-        openDoorImage = new Image((new File("images/open_door.png")).toURI().toString());
-        closedDoorImage = new Image((new File("images/closed_door.png")).toURI().toString());
-        keyImage = new Image((new File("images/key.png")).toURI().toString());
-        boulderImage = new Image((new File("images/boulder.png")).toURI().toString());
-        switchImage = new Image((new File("images/pressure_plate.png")).toURI().toString());
-        portalImage = new Image((new File("images/portal.png")).toURI().toString());
-        enemyImage = new Image((new File("images/deep_elf_master_archer.png")).toURI().toString());
-        swordImage = new Image((new File("images/greatsword_1_new.png")).toURI().toString());
-        invincibilityImage = new Image((new File("images/brilliant_blue_new.png")).toURI().toString());
     }
 
     public DungeonControllerLoader(File file) throws FileNotFoundException {
         super(file);
-        entities = new ArrayList<>();
-        playerImage = new Image((new File("images/human_new.png")).toURI().toString());
-        wallImage = new Image((new File("images/brick_brown_0.png")).toURI().toString());
-        exitImage = new Image((new File("images/exit.png")).toURI().toString());
-        treasureImage = new Image((new File("images/gold_pile.png")).toURI().toString());
-        openDoorImage = new Image((new File("images/open_door.png")).toURI().toString());
-        closedDoorImage = new Image((new File("images/closed_door.png")).toURI().toString());
-        keyImage = new Image((new File("images/key.png")).toURI().toString());
-        boulderImage = new Image((new File("images/boulder.png")).toURI().toString());
-        switchImage = new Image((new File("images/pressure_plate.png")).toURI().toString());
-        portalImage = new Image((new File("images/portal.png")).toURI().toString());
-        enemyImage = new Image((new File("images/deep_elf_master_archer.png")).toURI().toString());
-        swordImage = new Image((new File("images/greatsword_1_new.png")).toURI().toString());
-        invincibilityImage = new Image((new File("images/brilliant_blue_new.png")).toURI().toString());
     }
 
     @Override
