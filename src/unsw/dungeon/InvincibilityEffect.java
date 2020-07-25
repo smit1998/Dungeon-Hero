1ca71dc3d56f4194e5cf258c1e7edba23765ecc1
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-07-25 15:38:02 +1000

    added skeletons for invisibility, fear and invicibility effectBehaviours

---
+++ src/unsw/dungeon/InvincibilityEffect.java
@@ -0,0 +1,17 @@
+package unsw.dungeon;
+
+public class InvincibilityEffect implements EffectBehaviour {
+
+    @Override
+    public void startEffect() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void stopEffect() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
