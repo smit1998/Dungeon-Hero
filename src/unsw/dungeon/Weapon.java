39782ccbe4f2bc16f52f91a059569fceea2383b8
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-07-25 22:20:18 +1000

    changed weapon to an extendable class

--- src/unsw/dungeon/Weapon.java
+++ src/unsw/dungeon/Weapon.java
@@ -3,17 +3,22 @@ package unsw.dungeon;
 /**
  * Interface for a weapon, used for items that can be used to attack enemies
  */
-public interface Weapon {
-    /**
-     * @return the priority of a weapon, a number from 0 - Integer.MAX_VALUE the
-     *         higher the number the higher the priority of the weapon
-     */
-    public int getPriority();
+public class Weapon extends ItemEntity {
+
+    private AttackBehaviour attackBehaviour; 
+    private int hitsRemaining;  
+
+    public Weapon(int x, int y, Dungeon dungeon, AttackBehaviour attackBehaviour, int hitsRemaining) {
+        super(x, y, dungeon); 
+        this.attackBehavior = attackBehaviour;  
+    }
 
     /**
      * attacks an enemy, with unique interaction depending on the weapon used
      * 
      * @param the enemy to be attacked
      */
-    public void attack(LifeEntity e);
+    public void attack(LifeEntity e) {
+        attackBehaviour.attack(e); 
+    }
 }
