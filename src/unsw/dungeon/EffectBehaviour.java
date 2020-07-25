fb6edbd8ee238f81e4ef14f828a02d88e60a804b
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-07-25 14:20:58 +1000

    added interface for effect, moved files into the right folder

---
+++ src/unsw/dungeon/EffectBehaviour.java
@@ -0,0 +1,5 @@
+package unsw.dungeon; 
+
+public interface EffectBehaviour {
+    public void effect(); 
+}
---
+++ src/unsw/dungeon/Potion.java
@@ -0,0 +1,13 @@
+package unsw.dungeon;
+
+import unsw.dungeon.ItemEntity;
+
+public class Potion extends ItemEntity {
+
+    public Potion(int x, int y, Dungeon dungeon, EffectBehaviour effect) {
+        super(x, y, dungeon);
+    }
+
+    public initiateEffect
+    
+}
