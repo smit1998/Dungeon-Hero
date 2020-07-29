78de8a5ff4ee14f3775cf981a16bf620881a0da9
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-07-29 17:57:14 +1000

    java docs for attack behaviour

--- src/unsw/dungeon/AttackBehaviour.java
+++ src/unsw/dungeon/AttackBehaviour.java
@@ -1,5 +1,11 @@
 package unsw.dungeon;
 
+/** 
+*   interface that represents a unique attack behaviour
+*/
 public interface AttackBehaviour {
+    /**
+     * method that can be called to attack a life entity
+     */
     public void attack(LifeEntity lifeEntity);     
 }
