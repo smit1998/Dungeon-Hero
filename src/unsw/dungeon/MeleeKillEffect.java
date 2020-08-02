54fc95b5baf5eaab4c9104f0dda3b5c1dc89b500
Fergus Yip <z5248282@unsw.edu.au>
2020-08-03 00:04:40 +1000

    Fix use of weapon when player has potion

--- src/unsw/dungeon/MeleeKillEffect.java
+++ src/unsw/dungeon/MeleeKillEffect.java
@@ -3,25 +3,26 @@ package unsw.dungeon;
 
 public class MeleeKillEffect implements EffectBehaviour {
 
-    private Inventory inventory; 
-    private Weapon oldWeapon; 
-    private Weapon meleeKillWeapon; 
+    private Inventory inventory;
+    private Weapon oldWeapon;
+    private Weapon meleeKillWeapon;
 
     public MeleeKillEffect(Dungeon dungeon) {
-        meleeKillWeapon = new Weapon(0, 0, dungeon, new InstantKillAttack(), Integer.MAX_VALUE); 
-        inventory = dungeon.getInventory();  
+        meleeKillWeapon = new Weapon(0, 0, dungeon, new InstantKillAttack(), Integer.MAX_VALUE);
+        inventory = dungeon.getInventory();
     }
 
     @Override
     public void startEffect() {
-        oldWeapon = inventory.getWeapon();  
+        oldWeapon = inventory.getWeapon();
+        inventory.removeItem(oldWeapon);
         inventory.addItem(meleeKillWeapon);
     }
 
     @Override
     public void stopEffect() {
-        inventory.removeItem(meleeKillWeapon); 
-        inventory.addItem(oldWeapon); 
+        inventory.removeItem(meleeKillWeapon);
+        inventory.addItem(oldWeapon);
     }
-    
+
 }
