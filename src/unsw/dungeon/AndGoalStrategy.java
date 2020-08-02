674c3620ba91fd6335d98d84f12a84cc46a8f259
duy minh nguyen <minh.nguyen19997@hotmail.com>
2020-08-02 11:46:55 +1000

    iterator pattern implemented in isComplete

--- src/unsw/dungeon/AndGoalStrategy.java
+++ src/unsw/dungeon/AndGoalStrategy.java
@@ -1,5 +1,6 @@
 package unsw.dungeon;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -14,13 +15,12 @@ public class AndGoalStrategy implements GoalStrategy {
      * @param goals a list of goals
      * @return whether the list of goals meet the requirements of this strategy
      */
-    public boolean isComplete(List<ComponentGoal> goals) {
-        for (ComponentGoal goal : goals) {
-            if (goal.isComplete() == false) {
-                return false;
-            }
+    public boolean isComplete(Iterator<ComponentGoal> goals) {
+        while (goals.hasNext()) {
+            ComponentGoal goal = goals.next(); 
+            if (!goal.isComplete()) { return false; }
         }
-        return true;
+        return true; 
     }
 
     public String toString(List<ComponentGoal> goals) {
